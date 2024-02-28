acfe5c98a9a75b698c465119082a84f2a39d70be
Alma <magnanialma03@gmail.com>
2024-02-28 09:30:23 +0100

    dtoOKAlma

--- src/main/java/com/generation/jamanjibackend/dto/dishtodelivery/DishtodeliveryDtoBase.java
+++ src/main/java/com/generation/jamanjibackend/dto/dishtodelivery/DishtodeliveryDtoBase.java
@@ -1,5 +1,15 @@
 package com.generation.jamanjibackend.dto.dishtodelivery;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 public class DishtodeliveryDtoBase {
 
+    private int quantity;
 }
