8eae60d7b693ac1ce12d82bd722486359763ac0e
Alma <magnanialma03@gmail.com>
2024-02-28 09:28:49 +0100

    dtoo

---
+++ src/main/java/com/generation/jamanjibackend/dto/dishtodelivery/DishtodeliveryDtoR.java
@@ -0,0 +1,14 @@
+package com.generation.jamanjibackend.dto.dishtodelivery;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DishtodeliveryDtoR extends DishtodeliveryDtoBase{
+
+    private Integer id;
+}
