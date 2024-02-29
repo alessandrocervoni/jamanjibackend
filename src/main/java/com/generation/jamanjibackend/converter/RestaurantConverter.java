6e4a86809e6568172652d2a4c4a8da088c961b0b
Alma <magnanialma03@gmail.com>
2024-02-29 10:09:24 +0100

    controller rest ok

--- src/main/java/com/generation/jamanjibackend/converter/RestaurantConverter.java
+++ src/main/java/com/generation/jamanjibackend/converter/RestaurantConverter.java
@@ -22,4 +22,26 @@ public class RestaurantConverter {
                 .build();
     }
 
+    public RestaurantDtoWFull RestoToDtoFull (Restaurant r){
+
+        return  RestaurantDtoWFull
+                .builder()
+                .id(r.getId())
+                .name(r.getName())
+                .phone(r.getPhone())
+                .openingHour(r.getOpeningHour())
+                .closingHour(r.getClosingHour())
+                .deliveryPricePerUnit(r.getDeliveryPricePerUnit())
+                .maxDeliveryDistance(r.getMaxDeliveryDistance())
+                .positionX(r.getPositionX())
+                .positionY(r.getPositionY())
+                .imgUrl(r.getImgUrl())
+                .isOpen(r.isOpen())
+                .foodTypes(r.getFoodTypes())
+                .distance(r.distance())
+                .menu(r.getMenu())
+                .build();
+    }
+
+
 }
--- src/main/java/com/generation/jamanjibackend/dto/restaurant/RestaurantDtoWFull.java
+++ src/main/java/com/generation/jamanjibackend/dto/restaurant/RestaurantDtoWFull.java
@@ -7,14 +7,19 @@ import com.generation.jamanjibackend.entities.Menu;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 
 public class RestaurantDtoWFull extends RestaurantDtoBase{
 
+    private Integer id;
     private Menu menu;
     private List<String> foodTypes;
+    private String isOpen;
+    private double distance;
 
 }
